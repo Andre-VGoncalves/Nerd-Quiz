402b2fdb3bc5461c657322704b2147c1197b51a5
Andre Goncalves <andre.vgoncalves@hotmail.com>
2018-09-26 00:38:09 -0300

    tela de login e cadastro de usuario feito

---
+++ app/src/main/java/com/example/andre/questquiz/ui/activity/CadastroUserActivity.java
@@ -0,0 +1,15 @@
+package com.example.andre.questquiz.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.andre.questquiz.R;
+
+public class CadastroUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_user);
+    }
+}
