5524cc3a99da5db8f81e8ca0605a4ff8344e3b4e
MatheusEleodoro <matheuseleodoro@gmail.com>
2018-10-14 21:49:16 -0300

    - Implementado Popup menu em cada botão - Implementada Floting Button

--- app/src/main/java/com/example/andre/questquiz/ui/activity/MainActivity.java
+++ app/src/main/java/com/example/andre/questquiz/ui/activity/MainActivity.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.PopupMenu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.andre.questquiz.R;
 import com.example.andre.questquiz.ui.activity.fragment.HomeFragment;
@@ -11,7 +15,9 @@ import com.example.andre.questquiz.ui.activity.fragment.MenuFragment;
 import com.example.andre.questquiz.ui.activity.fragment.StatsFragment;
 import com.example.andre.questquiz.ui.activity.fragment.ViewPageAdapter;
 
-public class MainActivity extends AppCompatActivity {
+import static android.widget.Toast.*;
+
+public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
     private TabLayout tabLayout;
     private ViewPager viewPager;
     int [] img_tab = {R.drawable.home_icon,R.drawable.conquista_icon,R.drawable.perfil_icon};
@@ -37,4 +43,34 @@ public class MainActivity extends AppCompatActivity {
             tab.setIcon(img_tab[i]);
         }
     }
+
+    public void showPopup(View v){
+        PopupMenu popup = new PopupMenu(this,v);
+        popup.setOnMenuItemClickListener(this);
+        popup.inflate(R.menu.popup_menu);
+        popup.show();
+
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        //Implentar aqui metodo para indentificar qual botão chamou
+        switch (item.getItemId()){
+            case R.id.item1:
+               return true;
+            case R.id.item2:
+                return true;
+            case R.id.item3:
+                return true;
+            case R.id.item4:
+                return true;
+                default:
+                    return false;
+        }
+    }
+    public void openCadastroPerguntas(){
+        //Implentar aqui comando para abrir tela de cadastro para as perguntas
+        
+    }
+
 }
